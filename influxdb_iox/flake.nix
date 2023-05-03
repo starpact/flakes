e061704f4808ffa012960fef0de7cf22a033cdee
starpact <yhj.await@gmail.com>
2023-05-03 17:24:05 +0800

    _

---
+++ influxdb_iox/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      llvmPackages = pkgs.llvmPackages_15;
+      stdenv = llvmPackages.stdenv;
+    in
+    {
+      devShells.default = pkgs.mkShell.override { inherit stdenv; } {
+        buildInputs = with pkgs; [
+          lld_15
+          openssl
+          pkg-config
+        ];
+
+        LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+        RUST_BACKTRACE = 1;
+      };
+    }
+  );
+}
